84acf70020e1e57eee352a942dc661f93e43e557
Fei Xue <fxnotepad@gmail.com>
2016-08-03 13:49:46 +0800

    Add CertHelper and TokenHelper class

---
+++ ADALSamples/CertHelper.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADALSamples
+{
+    class CertHelper
+    {
+
+        public static X509Certificate2 FindCert(string thumbprint)
+        {
+            return FindCert(thumbprint, "MY", StoreLocation.CurrentUser);
+        }
+
+        public static X509Certificate2 FindCert(string thumbprint, string storeName, StoreLocation storeLocation)
+        {
+            thumbprint = thumbprint.Replace("\u200e", string.Empty).Replace("\u200f", string.Empty).Replace(" ", string.Empty);
+            X509Store store = new X509Store(storeName, storeLocation);
+            store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+            X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
+            X509Certificate2Collection fcollection = (X509Certificate2Collection)collection.Find(X509FindType.FindByThumbprint, thumbprint, false);
+            return fcollection[0];
+        }
+
+        public static X509Certificate2 LoadCertFromFile(string filePath)
+        {
+            X509Certificate2 x509 = new X509Certificate2();
+            x509.Import(filePath);
+            return x509;
+        }
+
+        public static byte[] SignWithCertificate(string message, X509Certificate2 certificate)
+        {
+            if (certificate.PublicKey.Key.KeySize < ClientAssertionCertificate.MinKeySizeInBits)
+            {
+                throw new ArgumentOutOfRangeException("rawData", string.Format(CultureInfo.InvariantCulture, "The certificate used must have a key size of at least {0} bits", new object[]
+                {
+            ClientAssertionCertificate.MinKeySizeInBits
+                }));
+            }
+            RSACryptoServiceProvider rSACryptoServiceProvider = new X509AsymmetricSecurityKey(certificate).GetAsymmetricAlgorithm("http://www.w3.org/2001/04/xmldsig-more#rsa-sha256", true) as RSACryptoServiceProvider;
+            RSACryptoServiceProvider rSACryptoServiceProvider2 = null;
+            byte[] result;
+            try
+            {
+                rSACryptoServiceProvider2 = GetCryptoProviderForSha256(rSACryptoServiceProvider);
+                using (SHA256Cng sHA256Cng = new SHA256Cng())
+                {
+                    result = rSACryptoServiceProvider2.SignData(Encoding.UTF8.GetBytes(message), sHA256Cng);
+                }
+            }
+            finally
+            {
+                if (rSACryptoServiceProvider2 != null && rSACryptoServiceProvider != rSACryptoServiceProvider2)
+                {
+                    rSACryptoServiceProvider2.Dispose();
+                }
+            }
+            return result;
+        }
+
+        private static RSACryptoServiceProvider GetCryptoProviderForSha256(RSACryptoServiceProvider rsaProvider)
+        {
+            if ((rsaProvider.CspKeyContainerInfo.ProviderType == 1 || rsaProvider.CspKeyContainerInfo.ProviderType == 12) && !rsaProvider.CspKeyContainerInfo.HardwareDevice)
+            {
+                CspParameters cspParameters = new CspParameters
+                {
+                    ProviderType = 24,
+                    KeyContainerName = rsaProvider.CspKeyContainerInfo.KeyContainerName,
+                    KeyNumber = (int)rsaProvider.CspKeyContainerInfo.KeyNumber
+                };
+                if (rsaProvider.CspKeyContainerInfo.MachineKeyStore)
+                {
+                    cspParameters.Flags = CspProviderFlags.UseMachineKeyStore;
+                }
+                cspParameters.Flags |= CspProviderFlags.UseExistingKey;
+                return new RSACryptoServiceProvider(cspParameters);
+            }
+            return rsaProvider;
+        }
+
+        public static bool Verify(string message, byte[] signature, X509Certificate2 cert)
+        {
+            RSACryptoServiceProvider rSACryptoServiceProvider = cert.PublicKey.Key as RSACryptoServiceProvider;
+            RSACryptoServiceProvider rSACryptoServiceProvider2 = null;
+            bool isVerified = false;
+            try
+            {
+                rSACryptoServiceProvider2 = GetCryptoProviderForSha256(rSACryptoServiceProvider);
+                using (SHA256Cng sHA256Cng = new SHA256Cng())
+                {
+                    isVerified = rSACryptoServiceProvider2.VerifyData(Encoding.UTF8.GetBytes(message), sHA256Cng, signature);
+                }
+            }
+            finally
+            {
+                if (rSACryptoServiceProvider2 != null && rSACryptoServiceProvider != rSACryptoServiceProvider2)
+                {
+                    rSACryptoServiceProvider2.Dispose();
+                }
+            }
+            return isVerified;
+        }
+
+        public static bool Verify(byte[] message, byte[] signature, X509Certificate2 cert)
+        {
+            RSACryptoServiceProvider rSACryptoServiceProvider = cert.PublicKey.Key as RSACryptoServiceProvider;
+            RSACryptoServiceProvider rSACryptoServiceProvider2 = null;
+            bool isVerified = false;
+            try
+            {
+                rSACryptoServiceProvider2 = GetCryptoProviderForSha256(rSACryptoServiceProvider);
+                using (SHA256Cng sHA256Cng = new SHA256Cng())
+                {
+                    isVerified = rSACryptoServiceProvider2.VerifyData(message, sHA256Cng, signature);
+                }
+            }
+            finally
+            {
+                if (rSACryptoServiceProvider2 != null && rSACryptoServiceProvider != rSACryptoServiceProvider2)
+                {
+                    rSACryptoServiceProvider2.Dispose();
+                }
+            }
+            return isVerified;
+        }
+
+        public static byte[] FromBase64Url(string base64Url)
+        {
+            string padded = base64Url.Length % 4 == 0
+                ? base64Url : base64Url + "====".Substring(base64Url.Length % 4);
+            string base64 = padded.Replace("_", "/")
+                                  .Replace("-", "+");
+            return Convert.FromBase64String(base64);
+        }
+
+        public static string ConvertToBase64Url(string base64)
+        {
+            //char Base64Character62 = '+';
+            //char Base64Character63 = '/';
+            //char Base64UrlCharacter62 = '-';
+            //char Base64UrlCharacter63 = '_';
+            base64 = base64.Replace("=", "");
+            string base64Url = base64.Replace("/", "_")
+                            .Replace("+", "-");
+            return base64Url;
+
+        }
+
+        public static string ConvertToBase64(string base64Url)
+        {
+            string padded = base64Url.Length % 4 == 0
+                ? base64Url : base64Url + "====".Substring(base64Url.Length % 4);
+            string base64 = padded.Replace("_", "/")
+                                  .Replace("-", "+");
+            return base64;
+        }
+
+    }
+}
---
+++ ADALSamples/TokenHelp.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ADALSamples
+{
+    class TokenHelper
+    {
+        static string authority= "https://login.microsoftonline.com/common";
+        public static async Task<string> GetTokenByCert(string clientId, string tenant, string certThumbprint)
+        {
+
+            string apiResourceId = "https://manage.office.com";
+            string authority = $"https://login.windows.net/{tenant}";
+
+            X509Certificate2 cert = CertHelper.FindCert(certThumbprint);
+            var certCred = new ClientAssertionCertificate(clientId, cert);
+            var authContext = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(authority);
+            AuthenticationResult result = null;
+            try
+            {
+                result = await authContext.AcquireTokenAsync(apiResourceId, certCred);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return result.AccessToken;
+        }
+
+        public static async Task<string> GetTokenAsync(string resource, string clientId, string secrect)
+        {
+            string authority = "https://login.microsoftonline.com/O365e3w15.onmicrosoft.com";
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+
+            ClientCredential clientCredential = new ClientCredential(clientId, secrect);
+            AuthenticationResult authResult=await authContext.AcquireTokenAsync(resource, clientCredential);
+            return authResult.AccessToken;
+        }
+      
+        public static string GetToken(string resource, string clientId, UserCredential userCredential)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            AuthenticationResult ar = authContext.AcquireTokenAsync(resource, clientId, userCredential).Result;
+            return ar.AccessToken;
+        }
+
+        /// <summary>
+        /// Version: 2.19.208020213
+        /// </summary>
+        /// <param name="resource"></param>
+        /// <param name="clientId"></param>
+        /// <param name="userCredential"></param>
+        /// <returns></returns>
+        public static string GetTokenOld(string resource, string clientId, UserCredential userCredential)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            //AuthenticationResult ar = authContext.AcquireToken(resource, clientId, userCredential);// old version
+            PlatformParameters platformParameters = new PlatformParameters(PromptBehavior.Auto);
+            AuthenticationResult ar = authContext.AcquireTokenAsync(resource, clientId, userCredential).Result;
+            return ar.AccessToken;
+        }
+
+        public static string GetDeligateToken(string resource, string clientId,string redirectURL)
+        {
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            AuthenticationResult authResult= authContext.AcquireTokenAsync(resource, clientId,new Uri(redirectURL), new PlatformParameters(PromptBehavior.Auto)).Result;
+            return authResult.AccessToken;
+        }
+    }
+
+  
+}
