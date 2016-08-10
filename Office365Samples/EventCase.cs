803455e47d6f873f90375378f6af8af310b808eb
Fei Xue <fxnotepad@gmail.com>
2016-08-10 14:36:45 +0800

    Add demo: Update event with start and end property

--- Office365Samples/EventCase.cs
+++ Office365Samples/EventCase.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.OData.Client;
 
 namespace Office365Samples
 {
@@ -26,5 +27,53 @@ namespace Office365Samples
                 Console.WriteLine($"{appointment.Subject}:\t{appointment.Start}~{appointment.End}");
             }
         }
+
+        public static async void UpdateEvent(string accessToken)
+        {
+            OutlookServicesClient service = new OutlookServicesClient(new Uri("https://outlook.office.com/api/v2.0/"), () =>
+            {
+                return Task.Delay(10).ContinueWith(t => accessToken);
+            });
+
+
+            string meetingEventId = "AAMkADQyZjE2NzY3LWEyNjEtNGI3Ny05YmE5LWM3Yjk1N2RiZjg2YQBGAAAAAACjfX4Ad-CaQJG0NYun281fBwC90J0GawtZTaVMfIdaQJ8wAAAAAAENAAC90J0GawtZTaVMfIdaQJ8wAAFpdh5RAAA=";
+            bool updated;
+            try
+            {
+                var taskUpdateMeeting = Task<bool>.Run(async () =>
+                {
+                    bool updateStatus = false;
+                    IEvent meetingToUpdate = await service.Me.Events.GetById(meetingEventId).ExecuteAsync();
+                    if (meetingToUpdate != null)
+                    {
+                        
+                        DateTimeTimeZone newStart = new DateTimeTimeZone();
+                        newStart.DateTime= "2016-08-06T19:00:00.0000000";
+                        newStart.TimeZone = "Asia/Kolkata";
+
+
+                        DateTimeTimeZone newEnd = new DateTimeTimeZone();
+                        newEnd.DateTime = "2016-08-06T19:30:00.0000000";
+                        newEnd.TimeZone = "Asia/Kolkata";
+
+                        meetingToUpdate.Start = newStart;
+                        meetingToUpdate.End = newEnd;
+                     
+                        await meetingToUpdate.UpdateAsync(true);
+                        await meetingToUpdate.SaveChangesAsync();
+
+                        updateStatus = true;
+                    }
+                    return updateStatus;
+                });
+                Task.WaitAll(taskUpdateMeeting);
+                updated = taskUpdateMeeting.Result;
+            }
+            catch (Exception ex)
+            {
+                updated = false;
+            }
+        }
     }
+
 }
